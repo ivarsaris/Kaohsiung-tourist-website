e32b7b78755e230411c3b1e1e7a7e422f0681ba9
ivarsaris <ivarsaris@gmail.com>
2019-08-09 08:04:41 +0000

    moved javascript into own files

---
+++ js/restaurant-dropdown.js
@@ -0,0 +1,42 @@
+// dumpling restaurants
+function dumplingRestFunction() {
+    var x = document.getElementById("dumplingRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+// snow ice restaurants
+function iceRestFunction() {
+    var x = document.getElementById("iceRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+// stinky tofu restaurants
+function tofuRestFunction() {
+    var x = document.getElementById("tofuRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+// breakfast restaurants
+function breakfastRestFunction() {
+    var x = document.getElementById("breakfastRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
