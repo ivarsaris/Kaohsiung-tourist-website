5661deadbbc85ddef0f5fe53b63b8d51ba11cf4f
ivarsaris <ivarsaris@gmail.com>
2019-08-13 17:33:02 +0000

    fixed bug in email script

--- js/email.js
+++ js/email.js
@@ -1,18 +1,20 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "kaohsiung_tourist_website", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "days": contactForm.days.value,
-        "phone_number": contactForm.phone.value,
-        "traveling_from": contactForm.travelingFrom.value,
-        "budget": contactForm.budget.value,
-        "message": contactForm.message.value,
-    })
-    .then(
-        function(response) {
-            console.log("success", response);
-        },
-        function(error) {
-            console.log("fail!", error);
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "days": contactForm.days.value,
+            "phone_number": contactForm.phone.value,
+            "traveling_from": contactForm.travelingFrom.value,
+            "budget": contactForm.budget.value,
+            "message": contactForm.message.value,
+        })
+        .then(
+            function(response) {
+                console.log("success", response);
+            },
+            function(error) {
+                console.log("fail!", error);
         });
+        // to stop from reloading page
+        return false;
 }
