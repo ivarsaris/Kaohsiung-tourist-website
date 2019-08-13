9eeb6200fe60a916efa7dd114dad818d4cdb7683
ivarsaris <ivarsaris@gmail.com>
2019-08-13 09:29:32 +0000

    added contact form

---
+++ js/dropdowns.js
@@ -0,0 +1,164 @@
+// dumpling restaurants
+function dumplingRestFunction() {
+    var x = document.getElementById("dumplingRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+// snow ice restaurants
+function iceRestFunction() {
+    var x = document.getElementById("iceRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+// stinky tofu restaurants
+function tofuRestFunction() {
+    var x = document.getElementById("tofuRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+// breakfast restaurants
+function breakfastRestFunction() {
+    var x = document.getElementById("breakfastRestaurants");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+
+// things to do dropdowns
+function lotusFunction() {
+    var x = document.getElementById("lotus");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function qijinFunction() {
+    var x = document.getElementById("qijin");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function pierFunction() {
+    var x = document.getElementById("pier");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function zooFunction() {
+    var x = document.getElementById("zoo");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function ruifengFunction() {
+    var x = document.getElementById("ruifeng");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function buddhaFunction() {
+    var x = document.getElementById("buddha");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function liuheFunction() {
+    var x = document.getElementById("liuhe");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function beachFunction() {
+    var x = document.getElementById("beach");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function riverFunction() {
+    var x = document.getElementById("river");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function monkeyFunction() {
+    var x = document.getElementById("monkey");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function exhibitionFunction() {
+    var x = document.getElementById("exhibition");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function artscentreFunction() {
+    var x = document.getElementById("artscentre");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
