1ba2bda4323667336b285076ee90f4a246d71926
ivarsaris <ivarsaris@gmail.com>
2019-08-15 22:12:16 +0000

    added styling so dropdown sections come straight under head

--- js/restaurantdropdowns.js
+++ js/restaurantdropdowns.js
@@ -1,6 +1,6 @@
  function showRestaurant(element) {
 
-    // get all elements with class 'showRestaurants'
+    // get the dropdown content by getting the next sibling
     var restaurants = element.nextElementSibling;
 
    // if restaurants is visible
@@ -13,26 +13,3 @@
       restaurants.style.display = "block";
      }
  }
-
-  // function showRestaurantsFunction(restaurants) {
-
-  // }
-
-  // // when the DOM is loaded and rendered (HTML is rendered)
-  // document.addEventListener("DOMContentLoaded", function(event) {
-
-  //  // get all elements with class 'showRestaurants'
-  //  var restaurants = document.getElementsByClassName('showRestaurants');
-
-  //  // iterate through all "show restaurants"
-  //  for (var i = 0; i < restaurants.length; i++) {
-
-  //   // add a click event listener to each item
-  //   restaurants[i].addEventListener('click', function() {
-
-  //    // show or hide restaurants 
-  //    showRestaurantsFunction(restaurants);
-  //   });
-  //  }
-  // });
- 
