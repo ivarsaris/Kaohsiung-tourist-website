d2b90341d20cd66c38ee2815ab8a2fb509951766
ivarsaris <ivarsaris@gmail.com>
2019-08-18 09:15:13 +0000

    added comments to mail.js

--- js/email.js
+++ js/email.js
@@ -1,4 +1,7 @@
+// function which sends a filled out e-mail template to the website owner requestions a 
+// custom tailored holiday
 function sendMail(contactForm) {
+  // used emailjs, a free e-mail service
     emailjs.send("gmail", "kaohsiung_tourist_website", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.email.value,
